5948c6ec4e44ed9fd5aa88ec007fb3309ab5c700
abdelrahmanlghorab <abdoelghorab123@gmail.com>
2024-09-11 10:14:34 +0300

    intialize the structure

---
+++ src/app/watch-list-card/watch-list-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-watch-list-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './watch-list-card.component.html',
+  styleUrl: './watch-list-card.component.css'
+})
+export class WatchListCardComponent {
+
+}
