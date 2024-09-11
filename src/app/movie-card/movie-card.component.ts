3404489b840eaaf6d01513939ec10b9355f32a52
omarabuelkhier <omarabuelkhier@gmail.com>
2024-09-12 01:35:07 +0300

    Finishing Movies List Page UI and Connecting with API

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import { MoviesService } from '../services/movies.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-movie-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './movie-card.component.html',
   styleUrl: './movie-card.component.css'
 })
 export class MovieCardComponent {
+    movieService= inject(MoviesService)
+  isHoverd: boolean = false;
 
+
+@Input() movieData:any;
+@Output() sendToParent = new EventEmitter<number>();
+constructor(private router:Router){}
+  navigateToDetails(id: number) {
+    this.sendToParent.emit(id);
+    this.router.navigate([`/movie-details/${id}`]);
+}
+addToWatchList(movie:any){
+    this.movieService.addToWatchListArray(movie);
+    // console.warn(x);
+   }
+
+  hoverd() {
+    this.isHoverd = !this.isHoverd
+  }
 }
