f8c3811d0285c7bb6ff3ada845361235234c6160
MahmoudTtantawy <tantawym@gmail.com>
2024-09-12 17:20:21 +0300

    add recomandation component

--- src/app/movie-recomandation/movie-recomandation.component.ts
+++ src/app/movie-recomandation/movie-recomandation.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { MovieDetailService } from '../services/movie-detail.service';
+import { ActivatedRoute } from '@angular/router';
+import { MovieDetailComponent } from '../movie-detail/movie-detail.component';
 
 @Component({
   selector: 'app-movie-recomandation',
   standalone: true,
-  imports: [],
+  imports: [MovieDetailComponent],
   templateUrl: './movie-recomandation.component.html',
   styleUrl: './movie-recomandation.component.css'
 })
 export class MovieRecomandationComponent {
-
+  moviereco : any;
+  id :string= '';
+  isHoverd: boolean = false;
+  constructor(private movieRecomandationService: MovieDetailService,private activatedRoute: ActivatedRoute) {
+    this.id = this.activatedRoute.snapshot.params['id'];
+  }
+ 
+  ngOnInit() {
+    this.movieRecomandationService.getMovieRecomandation(this.id).subscribe((data: any) => {
+      this.moviereco = data.results;
+    });
+  }
+  hoverd() {
+    this.isHoverd = !this.isHoverd
+  }
 }
+
+
+
--- src/app/services/movie-detail.service.ts
+++ src/app/services/movie-detail.service.ts
@@ -10,5 +10,7 @@ export class MovieDetailService {
   getMovieDetails(id : string) {
     return this.http.get(`https://api.themoviedb.org/3/movie/${id}?api_key=b6d1b0c65bcc889f545646a926fb22b9`)
   }
+  getMovieRecomandation(id : string) {
+    return this.http.get(`https://api.themoviedb.org/3/movie/${id}/recommendations?api_key=b6d1b0c65bcc889f545646a926fb22b9`)
   }
-
+}
