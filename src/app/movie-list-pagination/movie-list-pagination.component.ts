428a19be4599ea14d59d4707620924a132906304
omarabuelkhier <omarabuelkhier@gmail.com>
2024-09-13 17:33:10 +0300

    finishing pagination logic

--- src/app/movie-list-pagination/movie-list-pagination.component.ts
+++ src/app/movie-list-pagination/movie-list-pagination.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter ,NgModule} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-movie-list-pagination',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './movie-list-pagination.component.html',
   styleUrl: './movie-list-pagination.component.css'
 })
 export class MovieListPaginationComponent {
+
+  @Input() currentPage: number = 1;   // Input for the current page
+  @Input() total_pages: number = 1;     // Input for the total number of pages
+  @Output() pageChange = new EventEmitter<number>();  // Output to emit page change event
+CommonModule: any;
+
+  // Method to navigate to the previous page
+  goToPreviousPage(): void {
+    if (this.currentPage > 1) {
+      this.pageChange.emit(this.currentPage - 1);
+    }
+  }
+
+  // Method to navigate to the next page
+  goToNextPage(): void {
+    if (this.currentPage < this.total_pages) {
+      this.pageChange.emit(this.currentPage + 1);
+    }
+  }
+
+  // Method to handle direct page selection
+  goToPage(page: number): void {
+    if (page >= 1 && page <= this.total_pages) {
+      this.pageChange.emit(page);
+    }
+  }
+  // Method to generate visible page numbers
+  getVisiblePages(): number[] {
+    let pages = [];
+    const start = Math.max(1, this.currentPage - 2);
+    const end = Math.min(this.total_pages, this.currentPage + 2);
+
+    for (let i = start; i <= end; i++) {
+      pages.push(i);
+    }
+
+    return pages;
+  }
 
 }
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -3,33 +3,44 @@ import { MoviesService } from '../services/movies.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { RouterLink } from '@angular/router';
-import { NgbRatingConfig, NgbRatingModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchComponent } from "../search/search.component";
 import Movie from '../interface';
+
 import { observableToBeFn } from 'rxjs/internal/testing/TestScheduler';
+import { MovieListPaginationComponent } from '../movie-list-pagination/movie-list-pagination.component';
 
 
 @Component({
   selector: 'app-movie-list',
   standalone: true,
-  imports: [FontAwesomeModule, MovieCardComponent, RouterLink, NgbRatingModule, SearchComponent],
+  imports: [FontAwesomeModule, MovieCardComponent, RouterLink, SearchComponent,MovieListPaginationComponent],
   templateUrl: './movie-list.component.html',
   styleUrl: './movie-list.component.css'
 })
 export class MovieListComponent {
+  currentPage: number = 1;
+  total_pages: number=1;
+  pageSize: number = 20;
 
-  starConfig: any;
+
+
   @Input() rating: number = 0;
 
-  moviesList: Movie[]= [];
+  moviesList: Movie[]=[];
   constructor(private movieService: MoviesService) {
   }
 
   ngOnInit() {
-    this.movieService.getMovies().subscribe((data:any) => (this.moviesList = data.results))
+    this.movieService.getMovies()
+      .subscribe((data:any) => (this.moviesList = data.results))
   }
   reciveFromChild(id: number) {
     console.log('FROM PARENT', id);
+  }
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.movieService.getMovies()
+
   }
 
 }
