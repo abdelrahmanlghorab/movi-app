09409348ec3084e5c6a6136de00728d0dea3575a
Abdelrahman1022 <abdoayman56@yahoo.com>
2024-09-13 20:47:56 +0200

    i created the search result page body , css and functions

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,49 +1,36 @@
-// import { Component, inject } from '@angular/core';
-// import { SearchService } from '../services/search.service';
-
-// @Component({
-//   selector: 'app-search-result',
-//   standalone: true,
-//   imports: [],
-//   templateUrl: './search-result.component.html',
-//   styleUrl: './search-result.component.css'
-// })
-// export class SearchResultComponent {
-// SearchResult=inject(SearchService);
-// movieSearchResult:any;
-// ngOnInit() {
-//   this.movieSearchResult = this.SearchResult.getSearchMovies().subscribe(res => {
-//     this.movieSearchResult = res;});
-// }
-// }
-
-
-import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { MovieCardComponent } from '../movie-card/movie-card.component';
+import { Component, inject } from '@angular/core';
+import { SearchService } from '../services/search.service';
+import { SearchComponent } from "../search/search.component";
+import { MovieCardComponent } from "../movie-card/movie-card.component";
+import { WatchListService } from '../services/watch-list.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-result',
   standalone: true,
-  imports: [CommonModule, MovieCardComponent],
+  imports: [SearchComponent, MovieCardComponent],
   templateUrl: './search-result.component.html',
-  styleUrls: ['./search-result.component.css'],
+  styleUrls: ['./search-result.component.css']
 })
-export class SearchResultComponent implements OnInit {
-  //#1 i created an array to hold all movies
-  results: any[] = [];
-  //#2 i created  an string value ,it will be the value of searchinput
-  query: string = '';
-  //#3 i created ACT_ROUTE to get query form url
-  constructor(private route: ActivatedRoute) { }
-  //#4 in ngOnInit  i subcribed  to get query and passes it to query_variable
-  // #5 then i checked if there results in url convert it an array or return an empty array
-  ngOnInit(): void {
-    this.route.queryParams.subscribe((params) => {
-      this.query = params['query'] || '';
-      this.results = params['results'] ? JSON.parse(params['results']) : [];
-      console.log(this.query, this.results);
+export class SearchResultComponent {
+  searchService = inject(SearchService);
+  keyword: string = '';
+  watchList: any[] = [];
+  watchListService = inject(WatchListService);
+  movieSearchResult: any;
+
+  constructor(private router: Router) {}
+
+  searchFn(key: string) {
+    this.searchService.getSearchResult(key);
+    this.router.navigate([`/search/${key}`]);
+  }
+
+  ngOnInit() {
+    this.searchService.getSearchMovies().subscribe(res => {
+      this.movieSearchResult = res;
+      console.log(this.movieSearchResult);
     });
+    this.keyword = this.searchService.getKey();
   }
 }
